f7eb14c12d0e381ee25c316973f1b6b6d602bffb
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-10-23 13:28:37 +0200

    Update RootLayout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,59 @@
-import { Stack } from 'expo-router';
+import { Slot, SplashScreen, useRouter, useSegments } from 'expo-router';
+import { useEffect } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {
+	Inter_300Light,
+	Inter_400Regular,
+	Inter_500Medium,
+	Inter_700Bold,
+	useFonts,
+} from '@expo-google-fonts/inter';
+import AuthProvider from 'services/providers/auth';
+import { useAuth } from 'hooks/useAuth';
+
+SplashScreen.preventAutoHideAsync();
+
+function MainLayout() {
+	const { isAuthenticated } = useAuth();
+	const segments = useSegments();
+	const router = useRouter();
+
+	useEffect(() => {
+		const inApp = segments[0] == '(app)';
+
+		if (isAuthenticated && !inApp) {
+			router.replace('/chats');
+		} else if (!isAuthenticated) {
+			router.replace('/signin');
+		}
+	}, [isAuthenticated]);
+
+	return <Slot />;
+}
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+	const [loaded, error] = useFonts({
+		Inter_300Light,
+		Inter_400Regular,
+		Inter_500Medium,
+		Inter_700Bold,
+	});
+
+	useEffect(() => {
+		if (loaded || error) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded, error]);
+
+	if (!loaded && !error) {
+		return null;
+	}
+
+	return (
+		<SafeAreaProvider>
+			<AuthProvider>
+				<MainLayout />
+			</AuthProvider>
+		</SafeAreaProvider>
+	);
 }
