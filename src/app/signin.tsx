d261947fcb20a4046f608304dc14911129f874bb
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-10-23 13:01:53 +0200

    Create SignIn page

---
+++ src/app/signin.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function SignInPage() {
+	return (
+		<View>
+			<Text>SignInPage</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({});
