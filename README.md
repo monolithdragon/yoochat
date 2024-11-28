39f0a71efbb01bc9d9201fa43909861344d57e75
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-11-28 11:54:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# YooChat 👋
+
+This is an realtime chat application with video call built with Expo.
+
+## Features
+
+- Realtime chat
+- User authentication
+- User profile
+- User settings
+- User search
+- User online status
+- User typing status
+- User last seen
+- Video call
---
+++ app.config.ts
@@ -0,0 +1,53 @@
+export default {
+  expo: {
+    name: 'YooChat',
+    slug: 'yoochat',
+    version: '1.0.0',
+    description: 'A real-time chat application built with Expo.',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'yoochat',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+      package: 'com.yoozone.yoochat',
+    },
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png',
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-splash-screen',
+        {
+          image: './assets/images/splash.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff',
+        },
+      ],
+      '@react-native-firebase/app',
+      '@react-native-firebase/auth',
+      '@react-native-firebase/crashlytics',
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      eas: {
+        projectId: 'a6f6fe9f-aa34-4b0b-87c1-0fe8ee39d0f4',
+      },
+    },
+    owner: 'monolithdragon',
+  },
+};
