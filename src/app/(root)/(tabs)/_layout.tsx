a7bf287291e51a00ce590a65b8884217702bf483
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-11-29 12:51:44 +0100

    Implement tabs

--- src/app/(root)/(tabs)/_layout.tsx
+++ src/app/(root)/(tabs)/_layout.tsx
@@ -1,23 +1,93 @@
+import { TabIcon } from '@/components/ui/tab_icon';
+import { colors } from '@/constants/colors';
+import {
+  ChatDots,
+  ChatDotsActive,
+  Menu,
+  UserCircle,
+  UserCircleActive,
+  UserGroup,
+  UserGroupActive,
+} from '@/constants/icons';
 import { Tabs } from 'expo-router';
+import { useColorScheme } from 'react-native';
 
 export default function TabsLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          height: 100,
+          backgroundColor: colorScheme === 'light' ? '#fff' : colors.neutral[800],
+          borderTopLeftRadius: 12,
+          borderTopRightRadius: 12,
+          borderWidth: 0,
+        },
+        tabBarInactiveTintColor: colors.neutral[300],
+        tabBarActiveTintColor: '#fff',
+      }}>
       <Tabs.Screen
         name='chats'
-        options={{ title: 'Chats' }}
+        options={{
+          title: '',
+          tabBarIcon: ({ color, focused, size }) => (
+            <TabIcon
+              name='Chats'
+              Icon={focused ? ChatDotsActive : ChatDots}
+              color={color}
+              size={size}
+              focused={focused}
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name='groups'
-        options={{ title: 'Groups' }}
+        options={{
+          title: '',
+          tabBarIcon: ({ color, focused, size }) => (
+            <TabIcon
+              name='Groups'
+              Icon={focused ? UserGroupActive : UserGroup}
+              color={color}
+              size={size}
+              focused={focused}
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name='profile'
-        options={{ title: 'Profile' }}
+        options={{
+          title: '',
+          tabBarIcon: ({ color, focused, size }) => (
+            <TabIcon
+              name='Profile'
+              Icon={focused ? UserCircleActive : UserCircle}
+              color={color}
+              size={size}
+              focused={focused}
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name='settings'
-        options={{ title: 'Settings' }}
+        options={{
+          title: '',
+          tabBarIcon: ({ color, focused, size }) => (
+            <TabIcon
+              name='Settings'
+              Icon={Menu}
+              color={color}
+              size={size}
+              focused={focused}
+            />
+          ),
+        }}
       />
     </Tabs>
   );
