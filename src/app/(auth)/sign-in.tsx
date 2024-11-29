47eee16ecb27fbb567369b150295730a198332d4
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-11-29 20:23:00 +0100

    Implement sign in

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,10 +1,38 @@
-import { Text } from 'react-native';
+import { AuthAnimatedBackground } from '@/components/auth/animated_background';
+import AuthContent from '@/components/auth/content';
+import { AuthHeader } from '@/components/auth/header';
+import { router } from 'expo-router';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+  View,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function SignIn() {
   return (
-    <SafeAreaView className='flex-1 container mx-auto bg-white dark:bg-[#292929]'>
-      <Text>SignIn</Text>
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={-70}>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <SafeAreaView className='flex h-full p-5 bg-white dark:bg-[#292929]'>
+          <AuthAnimatedBackground className='-right-[248]' />
+          <View className='w-full h-1/2'>
+            <AuthHeader
+              title='Login'
+              subtitle='Enter your mobile phone'
+              buttonText='Register'
+              onPress={() => router.push('/sign-up')}
+              className='flex-row'
+            />
+          </View>
+          <View className='w-full h-1/2'>
+            <AuthContent />
+          </View>
+        </SafeAreaView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
