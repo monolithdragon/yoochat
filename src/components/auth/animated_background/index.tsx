bbfd72bb4baa5646d4a7d4a872f7aab449f232cd
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-11-29 20:21:36 +0100

    Create animated background for auth

---
+++ src/components/auth/animated_background/index.tsx
@@ -0,0 +1,45 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import { useColorScheme } from 'react-native';
+import Animated, {
+  interpolate,
+  KeyboardState,
+  useAnimatedKeyboard,
+  useAnimatedStyle,
+  useDerivedValue,
+  withTiming,
+} from 'react-native-reanimated';
+
+type Props = {
+  className?: string;
+};
+
+export function AuthAnimatedBackground({ className }: Props) {
+  const keyboard = useAnimatedKeyboard();
+  const colorScheme = useColorScheme();
+
+  const progress = useDerivedValue(() => {
+    const isKeyboardVisible =
+      keyboard.state.value === KeyboardState.OPEN || keyboard.state.value === KeyboardState.OPENING;
+
+    return withTiming(isKeyboardVisible ? 1 : 0, {
+      duration: 100,
+    });
+  }, [keyboard.state.value]);
+
+  const animationStyle = useAnimatedStyle(() => ({
+    height: interpolate(progress.value, [0, 1], [1000, 350]),
+    top: interpolate(progress.value, [0, 1], [-647, -71]),
+    borderRadius: interpolate(progress.value, [0, 1], [500, 0]),
+  }));
+
+  return (
+    <Animated.View
+      className={`absolute w-[1000px] flex items-center justify-center overflow-hidden border-[60px] border-secondary-50 dark:border-neutral-800 ${className}`}
+      style={animationStyle}>
+      <LinearGradient
+        colors={colorScheme === 'light' ? ['#40C4FF', '#03A9F4'] : ['#1565C0', '#0F4888']}
+        className='w-full h-full '
+      />
+    </Animated.View>
+  );
+}
